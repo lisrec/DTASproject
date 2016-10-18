581d786feda1f6b845b38ae6f901c600a1155166
Krzysztof Palka <krzysztofpalka@c68-24.wmi.amu.edu.pl>
2016-10-18 17:01:31 +0200

    Angular2 with bootstrap initialized

---
+++ client/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'my-app',
+	templateUrl: 'template/main.html'
+})
+
+export class AppComponent{
+	constructor() {}
+}
