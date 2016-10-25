50b3cab4c61a6a1c20c76486e7b62af4deef50a6
Michał Kędzierski <s407232@labs.wmi.amu.edu.pl>
2016-10-25 17:04:28 +0200

    Michal comp

---
+++ client/app/components/about/aboutMichal.component.ts
@@ -0,0 +1,14 @@
+import { Component } 		from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'about',
+	templateUrl: 'aboutMichal.component.html',
+	styleUrls: [ 'aboutMichal.component.css' ]
+})
+
+export class AboutMichalComponent{
+	constructor(){}
+    
+    
+}
