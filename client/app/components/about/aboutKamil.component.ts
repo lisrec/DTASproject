5116f5e31180398ebbacd660041d62d1811b8be7
Kamil Trąbka <s407317@labs.wmi.amu.edu.pl>
2016-10-25 17:06:34 +0200

    Kamil component

---
+++ client/app/components/about/aboutKamil.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+	selector: 'about',
+	templateUrl: 'aboutKamil.component.html',
+	styleUrls: [ 'aboutKamil.component.css' ]
+	
+})
+export class AboutKamilComponent implements OnInit {
+	
+
+	constructor() {
+	}
+
+
+	ngOnInit(){
+	}
+
+}
