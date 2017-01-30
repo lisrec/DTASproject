68e3d8f3f06598c3b388c1ed6b31c209f2961c17
lisrec <lisrec94@gmail.com>
2017-01-30 22:16:18 +0100

    Rejestracja użytkownika

--- client/app/components/register/register.component.ts
+++ client/app/components/register/register.component.ts
@@ -1,14 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { md5 } from './md5';
+import { RegisterService } from '../../services/register.service';
+import { Router } from '@angular/router';
 
 @Component({
 	moduleId: module.id,
 	selector: 'register',
-	templateUrl: 'register.component.html'
+	templateUrl: 'register.component.html',
+	styleUrls: [ 'register.component.css' ],
+	providers: [ RegisterService ]
 })
 export class RegisterComponent implements OnInit {
-	constructor() {}
+
+	login: string = "";
+	pass: string = "";
+	error: boolean = false;
+	processingRegister: boolean = false;
+
+	constructor(private registerService: RegisterService,
+				private router: Router) {}
 
 	ngOnInit() {
 		
+	}
+
+	register() {
+
+		if(this.login == "" || this.pass == "") {
+			this.error = true;
+			return false;
+		}
+
+		this.processingRegister = true;
+
+		let newUser = {
+			login: "",
+			pass: ""
+		}
+
+		newUser.login = this.login;
+		newUser.pass = md5(this.pass);
+
+		console.log(newUser);
+
+		this.registerService.saveUser(newUser)
+			.subscribe(res => {
+				if(res.fail) {
+					this.error = true;
+					this.processingRegister = false;
+				} else {
+					this.error = false;
+					this.processingRegister = false;
+					this.router.navigate(['/login']);
+				}
+			});
 	}
 }
--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,72 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/profiles', function(req, res, next){
-	res.send("Profiles list");
+var conf = require('../config.js');
+
+var mongojs = require('mongojs');
+var db = mongojs(conf.connectionString, ['users']);
+
+function checkUserExist(login, next) {
+	db.users.find({"login": login}, function(err, usr){
+		if(err){
+			return false;
+		} else {
+			usr = usr[0];
+			if(typeof(usr) !== 'undefined') {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	});
+}
+
+router.post('/register', function(req, res, next){
+	let newProfile = {
+		"login": "",
+    	"pass": "",
+    	"admin": false
+	}
+
+	newProfile.login = req.body.login;
+	newProfile.pass = req.body.pass;
+
+	console.log(newProfile);
+
+	if((newProfile.login != "") && (newProfile.login) && (newProfile.login.length >= 3) && (newProfile.pass != "") && (newProfile.pass)) {
+
+		db.users.find({"login": newProfile.login}, function(err, usr){
+			if(err){
+				db.users.insert(newProfile, function(err, newUsr){
+					if(err){
+						res.send(err);
+					} else {
+						res.json(newUsr);
+					}
+				});
+			} else {
+				usr = usr[0];
+				if(typeof(usr) !== 'undefined') {
+					res.json({
+						"fail": "userExist"
+					});
+				} else {
+					db.users.insert(newProfile, function(err, newUsr){
+						if(err){
+							res.send(err);
+						} else {
+							res.json(newUsr);
+						}
+					});
+				}
+			}
+		});
+
+	} else {
+		res.json({
+			"fail": "badArgs"
+		});
+	}
 });
 
 module.exports = router;
