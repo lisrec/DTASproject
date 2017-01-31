bf873defdaed037b634851777768c58dbf8a7b73
michak645 <lisrec94@gmail.com>
2017-01-31 10:54:46 +0100

    Wypozyczone ksiazki w profilu uzytkownika

--- client/app/components/profile/profile.component.ts
+++ client/app/components/profile/profile.component.ts
@@ -1,13 +1,16 @@
-import { Component } 		from '@angular/core';
+import { Component }         from '@angular/core';
+import { Router } 		from '@angular/router';
 import { AuthService }	from '../../services/auth.service';
 import { ReviewsService }	from '../../services/reviews.service';
+import { RentService } from '../../services/rent.service';
 import { BooksService }	from '../../services/books.service';
 
 
 @Component({
 	moduleId: module.id,
 	selector: 'profile',
-	providers: [ ReviewsService, 
+	providers: [ ReviewsService,
+                 RentService, 
 				 BooksService ],
 	templateUrl: 'profile.component.html',
 	styleUrls: [ 'profile.component.css' ]
@@ -15,14 +18,19 @@ import { BooksService }	from '../../services/books.service';
 export class ProfileComponent {
 
 	user: any = {};
-	reviews: any = [];
+    reviews: any = [];
+	rents: any = [];
 	books_borrowed: any = [];
+    noRentedBooks: boolean = true;
 
 	constructor(private authService: AuthService,
-				private reviewsService: ReviewsService,
+                private reviewsService: ReviewsService,
+                private rentService: RentService,
+				private router: Router,
 				private booksService: BooksService) {
 
 		this.loadRevs();
+        this.loadRents();
 
     }
 
@@ -49,6 +57,53 @@ export class ProfileComponent {
                         });
                     });
 
+            });
+    }
+
+    loadRents() {
+
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        let that = this;
+
+        that.authService.getUser(currentUser)
+            .subscribe(usr => {
+                
+                let userId = usr._id;
+
+                that.rentService.getRent(userId)
+                    .subscribe(rents => {
+                        console.log(rents);
+                        if(!rents.fail) {
+                            if(rents.length == 0) {
+                                that.noRentedBooks = true;
+                            } else {
+                                that.rents = rents;
+                                that.rents.forEach((row, index, array) => {
+                                    that.booksService.getBook(row.bookId)
+                                        .subscribe(book => {
+                                            array[index].book = book;
+                                        });
+                                });
+                                that.noRentedBooks = false;
+                            }
+                        } else {
+                            that.noRentedBooks = true;
+                        }
+                    });
+
+            });
+    }
+
+    returnBook(rentId) {
+        let that = this;
+        this.rentService.remRent(rentId)
+            .subscribe(res => {
+                console.log(res);
+                that.rents.forEach((rent,index,arr) => {
+                    if(rent._id == rentId) {
+                        arr.splice(index, 1);
+                    }
+                });
             });
     }
 
